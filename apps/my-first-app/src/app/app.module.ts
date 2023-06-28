a1e4a0a096a46c725e8340272d1810c54c8ba5fb
Farid Nikman <fnikman@gmail.com>
2023-06-28 09:57:16 +0800

    lesson: 19 Using Custom Components

--- apps/my-first-app/src/app/app.module.ts
+++ apps/my-first-app/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TitleComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
