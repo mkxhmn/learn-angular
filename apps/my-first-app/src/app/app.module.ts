4e7accc87c21f7ff18e58e2d6e45219edd34c8af
Farid Nikman <fnikman@gmail.com>
2023-06-30 22:43:51 +0800

    feat: touch up
    learn on how to pass "children" to component, this is similar to _children_ from React and _slot_ from Svelte and Vue
    
    fix: responsive

--- apps/my-first-app/src/app/app.module.ts
+++ apps/my-first-app/src/app/app.module.ts
@@ -6,9 +6,16 @@ import { AppComponent } from './app.component';
 import { TitleComponent } from './title/title.component';
 import { ServerComponent } from './server/server.component';
 import { ServerCollectionsComponent } from './server-collections/server-collections.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, TitleComponent, ServerComponent, ServerCollectionsComponent],
+  declarations: [
+    AppComponent,
+    TitleComponent,
+    ServerComponent,
+    ServerCollectionsComponent,
+    CardComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
