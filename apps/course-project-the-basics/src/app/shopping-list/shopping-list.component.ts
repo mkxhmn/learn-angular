0d94a80d57c31e4f386cba83a0c6d5e9d16626d5
Farid Nikman <fnikman@gmail.com>
2023-07-06 10:22:22 +0800

    lesson: Adding a Shopping List Edit Section

--- apps/course-project-the-basics/src/app/shopping-list/shopping-list.component.ts
+++ apps/course-project-the-basics/src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient({
+      name: 'Ikan Merah',
+      amount: 1,
+    }),
+    new Ingredient({
+      name: 'Sup',
+      amount: 1,
+    }),
+  ];
 }
