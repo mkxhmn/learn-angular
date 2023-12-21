9c2cff666378f805fe6a8ed725a10016c88f92d2
Farid Nikman <fnikman@gmail.com>
2023-12-21 22:39:11 +0800

    lesson: 89 Recipe Data with Property Binding
    This update modifies the recipe-item component to dynamically render data passed on from the recipe-list component. The recipe-list has been cleaned up and each recipe-item is now loaded with data using 'ngFor' directive. In addition, 'NgForOf' has been added to the 'app.module.ts' to accommodate this new change.

--- apps/course-project-components-and-databinding/src/app/app.module.ts
+++ apps/course-project-components-and-databinding/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { NgIf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -23,7 +23,7 @@ import { NgIf } from '@angular/common';
     ShoppingListComponent,
     ShoppingEditComponent,
   ],
-  imports: [BrowserModule, FormsModule, NgIf],
+  imports: [BrowserModule, FormsModule, NgIf, NgForOf],
   providers: [],
   bootstrap: [AppComponent],
 })
