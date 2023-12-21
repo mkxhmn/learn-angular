181ad416e8f71f0b86d62194938904cad930d1d9
Farid Nikman <fnikman@gmail.com>
2023-12-21 16:30:32 +0800

    feat: 88 Adding Navigation with Event Binding and ngIf
    This commit includes updates for allowing navigation selection between the "Recipes" and "Shopping List" components. Changes include broadcasting selected feature from HeaderComponent and dynamically loading the corresponding component (Recipes or Shopping List) in AppComponent based on the broadcasted selection. Additionally, NgIf has been imported for conditional rendering of components.

--- apps/course-project-components-and-databinding/src/app/app.component.ts
+++ apps/course-project-components-and-databinding/src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { FeatureProps } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  loadedFeature: FeatureProps = 'recipe';
+
+  onNavigate(feature: FeatureProps) {
+    this.loadedFeature = feature;
+  }
+}
--- apps/course-project-components-and-databinding/src/app/header/header.component.ts
+++ apps/course-project-components-and-databinding/src/app/header/header.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+export type FeatureProps = 'recipe' | 'shopping-list';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output('feature') feature = new EventEmitter<FeatureProps>();
+
+  onSelect(selection: FeatureProps) {
+    this.feature.emit(selection);
+  }
+}
