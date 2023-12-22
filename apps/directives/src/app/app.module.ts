d72923883f076ec3f2a1e6928665ec65ffc45e97
Farid Nikman <fnikman@gmail.com>
2023-12-22 11:01:23 +0800

    lesson: 94 ngFor and ngIf Recap

--- apps/directives/src/app/app.module.ts
+++ apps/directives/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NgForOf, NgIf } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, NgForOf, NgIf],
   providers: [],
   bootstrap: [AppComponent],
 })
