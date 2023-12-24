b73626a3c8562bad62c5f4ef1edbf0deff51a4e0
Farid Nikman <fnikman@gmail.com>
2023-12-24 16:37:18 +0800

    lesson: 105 - 106 Building and Using a Dropdown Directive, Closing the Dropdown from Anywhere

--- apps/course-project-components-and-databinding/src/app/app.module.ts
+++ apps/course-project-components-and-databinding/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { NgForOf, NgIf } from '@angular/common';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -22,6 +23,7 @@ import { NgForOf, NgIf } from '@angular/common';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    DropdownDirective,
   ],
   imports: [BrowserModule, FormsModule, NgIf, NgForOf],
   providers: [],
