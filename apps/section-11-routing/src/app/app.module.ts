bba3a318bd4163fb9729fa2e06ebb32d3bfcfcd1
Farid Nikman <fnikman@gmail.com>
2023-12-26 16:33:33 +0800

    lesson: 148 Outsourcing the Route Configuration

--- apps/section-11-routing/src/app/app.module.ts
+++ apps/section-11-routing/src/app/app.module.ts
@@ -10,48 +10,9 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
 import { NgForOf, NgIf } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'users',
-    component: UsersComponent,
-    children: [
-      {
-        path: ':id/:name',
-        component: UserComponent,
-      },
-    ],
-  },
-  {
-    path: 'servers',
-    component: ServersComponent,
-    children: [
-      {
-        path: ':id',
-        component: ServerComponent,
-      },
-      {
-        path: ':id/edit',
-        component: EditServerComponent,
-      },
-    ],
-  },
-  {
-    path: 'not-found',
-    component: PageNotFoundComponent,
-  },
-  {
-    path: '**', // catch all path that is unknown to your app
-    redirectTo: '/not-found',
-  },
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -64,13 +25,7 @@ const routes: Routes = [
     ServerComponent,
     PageNotFoundComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RouterModule.forRoot(routes),
-    NgForOf,
-    NgIf,
-  ],
+  imports: [BrowserModule, FormsModule, NgForOf, NgIf, AppRoutingModule],
   providers: [ServersService],
   bootstrap: [AppComponent],
 })
